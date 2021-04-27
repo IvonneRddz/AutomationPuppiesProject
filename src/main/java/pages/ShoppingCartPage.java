a666fb1d97a107fc6613b5797033599d5632cce4
IvonneRddz <39936785+IvonneRddz@users.noreply.github.com>
2021-04-26 19:02:58 -0500

    Update ShoppingCartPage.java
    Implement highLighterMethod

--- src/main/java/pages/ShoppingCartPage.java
+++ src/main/java/pages/ShoppingCartPage.java
@@ -3,7 +3,9 @@ package pages;
 import java.util.List;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class ShoppingCartPage {
@@ -14,7 +16,19 @@ public class ShoppingCartPage {
 	By btnChangeYourMind = By.xpath("//input[contains(@value,'Change')]");
 	By totalPrice = By.xpath("//*[@class='total_cell']");
 	
-
+	
+	public ShoppingCartPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+	public void highLighterMethod(By LocatorValue){
+		WebElement element= driver.findElement(LocatorValue);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('style', 'border: 2px solid red;');", element);
+	}
+	
+	
 	private static void delaySegundo(){
         try{
             Thread.sleep(3000);
@@ -24,24 +38,22 @@ public class ShoppingCartPage {
     }
 	
 	
-	public ShoppingCartPage(WebDriver driver) {
-		this.driver = driver;
-	}
-
-	
 	public void completeAdoption() {
+		highLighterMethod(btnCompleteAdoption);
 		driver.findElement(btnCompleteAdoption).click();
 		System.out.println("Step Description - Complete Adoption button was clicked");
 	}
 
 	
 	public void adoptAnother() {
+		highLighterMethod(btnAdoptAnother);
 		driver.findElement(btnAdoptAnother).click();
 		System.out.println("Step Description - Adopt Another button was clicked");
 	}
 
 	
 	public void changeYourMind() {
+		highLighterMethod(btnChangeYourMind);
 		driver.findElement(btnChangeYourMind).click();
 		System.out.println("Step Description - Change Your Mind button was clicked");
 		aceptAlert();
@@ -59,13 +71,16 @@ public class ShoppingCartPage {
 	
 
 	public String getTotalPrice() {
+		highLighterMethod(totalPrice);
 		return driver.findElement(totalPrice).getText();
 	}
 
 	
 	public void verifyPuppyDescription(String value) {
 			By puppyDescription = By.xpath("//*[contains(text(),'"+value+"')]");
+			highLighterMethod(puppyDescription);
 			String description = driver.findElement(puppyDescription).getText();
+			
 			Assert.assertEquals(value,description);
 			System.out.println("Step Description - Verified Puppy description");
 			System.out.println("Expected result: " + value + ", Actual result: " + description);
@@ -79,6 +94,8 @@ public class ShoppingCartPage {
 			for (int i = 0; i < list.size(); i++) {
 				String value = list.get(i);
 				By puppyName = By.xpath("//*[contains(text(),'" + value + "')]");
+				
+				highLighterMethod(puppyName);
 				Assert.assertEquals(list.get(i),driver.findElement(puppyName).getText()); 
 				summary += list.get(i) + ",";
 			}
@@ -88,6 +105,8 @@ public class ShoppingCartPage {
 	
 	public void verifyPuppyName(String value) {
 			By puppyName = By.xpath("//*[contains(text(),'" + value + "')]");
+			
+			highLighterMethod(puppyName);
 			Assert.assertEquals(value,driver.findElement(puppyName).getText()); 
 			System.out.println("Step Description - Verified Puppy Name");
 			System.out.println("Expected result: " + value + ", Actual result: " + driver.findElement(puppyName).getText() );
@@ -101,13 +120,20 @@ public class ShoppingCartPage {
 			By chkbToyAddicionalProduct = By.xpath("(//*[@id='toy'])["+i+"]");
 			By chkbCarrierAddicionalProduct = By.xpath("(//*[@id='carrier'])["+i+"]");
 			By chkbVetAddicionalProduct = By.xpath("(//*[@id='vet'])["+i+"]");
-
+			
+			highLighterMethod(chkbCollarAddicionalProduct);
 			driver.findElement(chkbCollarAddicionalProduct).click();
 			System.out.println("Step Description - Collar from Aditional products was selected, Puppy "+i);
+			
+			highLighterMethod(chkbToyAddicionalProduct);
 			driver.findElement(chkbToyAddicionalProduct).click();
 			System.out.println("Step Description - Toy from Aditional products was selected, Puppy "+i);
+			
+			highLighterMethod(chkbCarrierAddicionalProduct);
 			driver.findElement(chkbCarrierAddicionalProduct).click();
 			System.out.println("Step Description - Carrier from Aditional products was selected, Puppy "+i);
+			
+			highLighterMethod(chkbVetAddicionalProduct);
 			driver.findElement(chkbVetAddicionalProduct).click();
 			System.out.println("Step Description - Vet from Aditional products was selected, Puppy "+i);
 			}
@@ -124,10 +150,19 @@ public class ShoppingCartPage {
 			By carrierPrice = By.xpath("(//*[@class='carrier-amount'])["+i+"]");
 			By vetPrice = By.xpath("(//*[@class='vet-amount'])["+i+"]");
 			
+			highLighterMethod(puppyPrice);
 			String puppy = driver.findElement(puppyPrice).getText();
+			
+			highLighterMethod(collarPrice);
 			String collar = driver.findElement(collarPrice).getText();
+			
+			highLighterMethod(toyPrice);
 			String toy = driver.findElement(toyPrice).getText();
+			
+			highLighterMethod(carrierPrice);
 			String carrier = driver.findElement(carrierPrice).getText();
+			
+			highLighterMethod(vetPrice);
 			String vet = driver.findElement(vetPrice).getText();
 			
 			double pricePuppy = Double.parseDouble(puppy.replace("$", ""));
@@ -162,8 +197,10 @@ public class ShoppingCartPage {
 	
 	public String sumaItemPrice(int numberOfPeets) {
 		double suma = 0;
+		
 		for (int i = 1; i < numberOfPeets; i++) {
 			By itemPrice = By.xpath("(//*[@class='item_price'])[" + i + "]");
+			highLighterMethod(itemPrice);
 			String item = driver.findElement(itemPrice).getText();
 			String itemNew = item.replace("$", "");
 			double price = Double.parseDouble(itemNew);
@@ -176,6 +213,7 @@ public class ShoppingCartPage {
 	
 	public void verifyAdoptAmount(String value) {
 			By itemPrice = By.xpath("(//*[@class='item_price'])[1]");
+			highLighterMethod(itemPrice);
 			Assert.assertEquals(value,driver.findElement(itemPrice).getText());
 			System.out.println("Step Description: Verified Adopt Amount Puppy");
 			System.out.println("Expected result: " + value + ", Actual result: " + driver.findElement(itemPrice).getText());
@@ -192,3 +230,5 @@ public class ShoppingCartPage {
 		}
 	}
 }
+
+
