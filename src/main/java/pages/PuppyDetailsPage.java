88916fa9264676d5ed5f04d9a1ea32095d0a88a6
IvonneRddz <39936785+IvonneRddz@users.noreply.github.com>
2021-04-26 19:01:55 -0500

    Update PuppyDetailsPage.java
    Implement highLighterMethod

--- src/main/java/pages/PuppyDetailsPage.java
+++ src/main/java/pages/PuppyDetailsPage.java
@@ -1,18 +1,29 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PuppyDetailsPage {
 
 	WebDriver driver;
 	By btnAdoptMe = By.xpath("//input[contains(@value,'Adopt')]");
 
+	
 	public PuppyDetailsPage(WebDriver driver) {
 		this.driver = driver;
 	}
 	
+	public void highLighterMethod(By LocatorValue){
+		WebElement element= driver.findElement(LocatorValue);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('style', 'border: 2px solid red;');", element);
+	}
+	
+	
 	public void adoptMe() {
+		highLighterMethod(btnAdoptMe);
 		driver.findElement(btnAdoptMe).click();
 		System.out.println("Step Description - Adopt Me button was clicked");
 	}
